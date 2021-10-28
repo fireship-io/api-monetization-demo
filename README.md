d82227d686e44d2ff9547f25fbc18ced249f518a
Jeff Delaney <hello@fireship.io>
2021-10-28 08:10:25 -0700

    full working demo

---
+++ README.md
@@ -0,0 +1,18 @@
+## API Monetization Demo with Stripe Metered Billing
+
+Learn how to [make money from an API]() on YouTube. 
+
+Detailed breakdown of the [API monetization](https://fireship.io/lessons/api-monetization-stripe) code Fireship. 
+
+## Run it
+
+- Create a metered billing product in Stripe
+- Install Stripe CLI
+- Add your testing Stripe secret key in the index.js
+
+```bash
+git clone 
+npm i
+node .
+stripe listen --forward-to localhost:8080/webhook
+```
---
+++ index.js
@@ -0,0 +1,206 @@
+const express = require('express');
+const app = express();
+
+const stripe = require('stripe')('sk_test_YOUR-KEY');
+
+// Middleware required for Webhook Handler
+app.use(
+  express.json({
+    verify: (req, res, buffer) => (req['rawBody'] = buffer),
+  })
+);
+
+////// Data Model ///////
+
+// TODO Implement a real database
+// Reverse mapping of stripe to API key. Model this in your preferred database.
+const customers = {
+  // stripeCustomerId : data
+  stripeCustomerId: {
+    apiKey: '123xyz',
+    active: false,
+    itemId: 'stripeSubscriptionItemId',
+  },
+};
+const apiKeys = {
+  // apiKey : customerdata
+  '123xyz': 'cust1',
+};
+
+////// Custom API Key Generation & Hashing ///////
+
+// Recursive function to generate a unique random string as API key
+function generateAPIKey() {
+  const { randomBytes } = require('crypto');
+  const apiKey = randomBytes(16).toString('hex');
+  const hashedAPIKey = hashAPIKey(apiKey);
+
+  // Ensure API key is unique
+  if (apiKeys[hashedAPIKey]) {
+    generateAPIKey();
+  } else {
+    return { hashedAPIKey, apiKey };
+  }
+}
+
+// Hash the API key
+function hashAPIKey(apiKey) {
+  const { createHash } = require('crypto');
+
+  const hashedAPIKey = createHash('md5').update(apiKey).digest('hex');
+
+  return hashedAPIKey;
+}
+
+////// Express API ///////
+
+// Create a Stripe Checkout Session to create a customer and subscribe them to a plan
+app.post('/checkout', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    mode: 'subscription',
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price: 'price_YOUR-PRODUCT',
+      },
+    ],
+    // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+    // the actual Session ID is returned in the query parameter when your customer
+    // is redirected to the success page.
+    success_url:
+      'http://YOUR-WEBSITE/dashboard?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'http://YOUR-WEBSITE/error',
+  });
+
+  res.send(session);
+});
+
+// Listen to webhooks from Stripe when important events happen
+app.post('/webhook', async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  const webhookSecret = 'whsec_YOUR-KEY';
+
+  if (webhookSecret) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req['rawBody'],
+        signature,
+        webhookSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  switch (eventType) {
+    case 'checkout.session.completed':
+      console.log(data);
+      // Data included in the event object:
+      const customerId = data.object.customer;
+      const subscriptionId = data.object.subscription;
+
+      console.log(
+        `💰 Customer ${customerId} subscribed to plan ${subscriptionId}`
+      );
+
+      // Get the subscription. The first item is the plan the user subscribed to.
+      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+      const itemId = subscription.items.data[0].id;
+
+      // Generate API key
+      const { apiKey, hashedAPIKey } = generateAPIKey();
+      console.log(`User's API Key: ${apiKey}`);
+      console.log(`Hashed API Key: ${hashedAPIKey}`);
+
+      // Store the API key in your database.
+      customers[customerId] = {
+        apikey: hashedAPIKey,
+        itemId,
+        active: true,
+      };
+      apiKeys[hashedAPIKey] = customerId;
+
+      break;
+    case 'invoice.paid':
+      // Continue to provision the subscription as payments continue to be made.
+      // Store the status in your database and check when a user accesses your service.
+      // This approach helps you avoid hitting rate limits.
+      break;
+    case 'invoice.payment_failed':
+      // The payment failed or the customer does not have a valid payment method.
+      // The subscription becomes past_due. Notify your customer and send them to the
+      // customer portal to update their payment information.
+      break;
+    default:
+    // Unhandled event type
+  }
+
+  res.sendStatus(200);
+});
+
+// Get information about the customer
+app.get('/customers/:id', (req, res) => {
+  const customerId = req.params.id;
+  const account = customers[customerId];
+  if (account) {
+    res.send(account);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Make a call to the API
+app.get('/api', async (req, res) => {
+  const { apiKey } = req.query;
+
+  if (!apiKey) {
+    res.sendStatus(400); // bad request
+  }
+
+  const hashedAPIKey = hashAPIKey(apiKey);
+
+  const customerId = apiKeys[hashedAPIKey];
+  const customer = customers[customerId];
+
+  if (!customer || !customer.active) {
+    res.sendStatus(403); // not authorized
+  } else {
+
+    // Record usage with Stripe Billing
+    const record = await stripe.subscriptionItems.createUsageRecord(
+      customer.itemId,
+      {
+        quantity: 1,
+        timestamp: 'now',
+        action: 'increment',
+      }
+    );
+    res.send({ data: '🔥🔥🔥🔥🔥🔥🔥🔥', usage: record });
+  }
+});
+
+app.get('/usage/:customer', async (req, res) => {
+  const customerId = req.params.customer;
+  const invoice = await stripe.invoices.retrieveUpcoming({
+    customer: customerId,
+  });
+
+  res.send(invoice);
+});
+
+app.listen(8080, () => console.log('alive on http://localhost:8080'));
